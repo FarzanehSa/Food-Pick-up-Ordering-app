fddb1f40258222372d48fe9a105d5ed1ac4fffb3
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-04-25 19:00:44 -0700

    make orders route

---
+++ routes/orders.js
@@ -0,0 +1,24 @@
+/*
+* All routes for Orders are defined here
+* Since this file is loaded in server.js into orders,
+*   these routes are mounted onto /orders
+*/
+
+const express = require('express');
+// const {  } = require('../db/queries/database');
+const router  = express.Router();
+
+module.exports = (db) => {
+  // get orders/
+  // Rendering orders.ejs
+  router.get("/", (req, res) => {
+    const user = req.session.user;
+    if (!user)  {
+      res.redirect("/users");
+    }
+    res.render("orders", {user})
+  });
+
+  return router;
+};
+
