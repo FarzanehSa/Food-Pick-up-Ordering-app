fb8d4eb769f37c6b1376990a0e51cdd1b8090ce3
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-04-28 20:31:55 -0700

    change seeds for user

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,7 @@
 -- Users table seeds here
 
-INSERT INTO users (name, phone, password, access_level) VALUES ('Farzaneh', '111333' , '123', 1);
+INSERT INTO users (name, phone, password, access_level) VALUES ('Khaled', '111333' , '123', 1);
 INSERT INTO users (name, phone, password, access_level) VALUES ('Alice', '777888' , '123', 0);
 INSERT INTO users (name, phone, password, access_level) VALUES ('Tara', '888677' , '123', 0);
 INSERT INTO users (name, phone, password, access_level) VALUES ('Mike', '123432' , '123', 0);
+INSERT INTO users (name, phone, password, access_level) VALUES ('Farzaneh', '111333' , '123', 1);
