4313ea92d4c5e8bee0baf2fc306870990f82d8c4
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-04-30 18:36:02 -0400

    Add getOrderByUser to render ejs page

--- routes/my-orders.js
+++ routes/my-orders.js
@@ -7,10 +7,17 @@
 const express = require('express');
 const router = express.Router();
 
+const { getOrderByUser } = require('../db/queries/orders/03-getOrdersByUser');
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const user = req.session.user;
-    res.render("my-orders", { user });
+    getOrderByUser(db, user)
+    .then(data => {
+      const orders = data.rows;
+      // console.log(orders)
+      res.render('my-orders', {user, orders});
+    });
   });
   return router;
 };
